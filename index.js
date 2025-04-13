c89971bf548061455a1efa6c0e180bb3667c3ef3
Irakli <ambroladzeirakli@gmail.com>
2025-04-13 17:08:22 +0400

    timerId

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 const printGreeting = () => console.log("Hello");
-const timerId = setTimeout(printGreeting, 4_000);
 
-console.log("timerId", timerId);
-
-const clearedId = clearTimeout(timerId);
-
-console.log("clearedId", clearedId);
+setInterval(printGreeting, 2_000);
